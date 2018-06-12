5fc05012f5076c675534fe60c9d6762825486fb5
rvcwhitworth <rvcwhitworth@gmail.com>
2018-06-12 13:33:58 -0400

    add tests for question 3

--- question-3.js
+++ question-3.js
@@ -1,5 +1,5 @@
 /**
- * Question 3 -- changePossibilities(amount,amount): 
+ * Question 3 -- changePossibilities(amount,denominations): 
  * Your quirky boss collects rare, old coins. They found out you're a programmer 
  * and asked you to solve something they've been wondering for a long time. 
  *
@@ -16,3 +16,30 @@
  * 1¢, 3¢
  * 2¢, 2¢
  */
+
+const testCases = [
+  {
+    expected: 4,
+    actual: changePossibilities(4, [1, 2, 3])
+  },
+  {
+    expected: 1,
+    actual: changePossibilities(1, [1, 2, 3])
+  },
+  {
+    expected: 0,
+    actual: changePossibilities(5, [6])
+  }
+];
+
+function assert(expected, actual) {
+  if (expected === actual) {
+    console.log("PASSED");
+  } else {
+    console.log(`FAILED: Expected ${expected} but got ${actual}`);
+  }
+}
+
+testCases.forEach(({ expected, actual }) => {
+  assert(expected, actual);
+});
